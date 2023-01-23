d9d2607f191c3fe4c615f58af29006655c8eaffb
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-01-23 15:12:15 +0100

    iteration 5 movie model done

---
+++ models/Movie.js
@@ -0,0 +1,26 @@
+//  Add your code here
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const movieSchema = new Schema({
+  title:{
+    type: String,
+    required: true,
+    unique: true
+  },
+  genre:{
+    type:String,
+    required:true,
+  },
+  plot:{
+    type: String
+  },
+  cast:{
+    type: [Schema.Types.ObjectId],
+    ref: 'Celebrity'
+  }
+});
+
+const Movie = model("Movie", movieSchema);
+
+module.exports = Movie;
